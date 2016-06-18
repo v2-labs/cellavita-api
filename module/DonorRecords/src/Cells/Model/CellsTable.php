2dcadb777953702aade120aec750868d621dc530
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2016-06-17 21:48:10 -0300

    Fix a wrong namespace
    This commit fixes a wrong namespace on Cells submodule.

--- module/DonorRecords/src/Cells/Model/CellsTable.php
+++ module/DonorRecords/src/Cells/Model/CellsTable.php
@@ -7,7 +7,7 @@
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
-namespace Donors\Model;
+namespace Cells\Model;
 
 use Zend\Db\Sql\Select;
 use Zend\Db\Adapter\Adapter;
